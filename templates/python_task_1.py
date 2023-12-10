5a1f4c562a7d4d00d6773bf8beceb2a28bcbd7b8
arnabsangamdata <143175307+arnabsangamdata@users.noreply.github.com>
2023-12-10 23:24:42 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,8 +13,25 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+        
+    ids = sorted(set(df['id_1'].unique()) | set(df['id_2'].unique()))
 
-    return df
+    matrix = pd.DataFrame(np.zeros((len(ids), len(ids))), index=ids, columns=ids)
+
+    
+    for row in df.itertuples(index=False):
+        matrix.at[row.id_1, row.id_2] = row.car
+        matrix.at[row.id_2, row.id_1] = row.car
+
+  
+    np.fill_diagonal(matrix.values, 0)
+
+    return matrix
+
+result_matrix = generate_car_matrix(df)
+return result_matrix
+
+
 
 
 def get_type_count(df)->dict:
@@ -28,8 +45,23 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+    
 
-    return dict()
+   
+    data['car_type'] = pd.cut(data['car'], bins=[-float('inf'), 15, 25, float('inf')],
+                              labels=['low', 'medium', 'high'], right=False)
+
+    
+    type_counts = data['car_type'].value_counts().to_dict()
+
+   
+    sorted_type_counts = dict(sorted(type_counts.items()))
+
+    return sorted_type_counts
+result = get_type_count(df)
+return result
+
+    
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +75,15 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+     
+    bus_mean = df['bus'].mean()
+    bus_indices = df[df['bus'] > 2 * bus_mean].index.tolist()
+    bus_indices.sort()
+    return bus_indices
+result_indices = get_bus_indexes(df)
+return result_indices
 
-    return list()
+    
 
 
 def filter_routes(df)->list:
@@ -58,11 +97,20 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+        truck_mean = df['truck'].mean()
+    filtered_routes = df.groupby('route')['truck'].mean().reset_index()
+    filtered_routes = filtered_routes[filtered_routes['truck'] > 7]['route'].tolist()
 
-    return list()
+    filtered_routes.sort()
+
+    return filtered_routes
+result_routes = filter_routes(df)
+return result_routes
+
+ 
 
 
-def multiply_matrix(matrix)->pd.DataFrame:
+def multiply_matrix(result_matrix)->pd.DataFrame:
     """
     Multiplies matrix values with custom conditions.
 
@@ -73,8 +121,14 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+    
+    matrix = result_matrix.copy()
+    matrix = matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+    matrix = matrix.round(1)
 
     return matrix
+
+    
 
 
 def time_check(df)->pd.Series:
@@ -88,5 +142,15 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    df['start_datetime'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+    df['end_datetime'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+    df['duration'] = (df['end_datetime'] - df['start_datetime']).dt.total_seconds()
+    expected_duration = 24 * 60 * 60
+    df['incorrect_timestamp'] = ((df['duration'] < expected_duration) |
+                                  (df['start_datetime'].dt.day_name() != df['end_datetime'].dt.day_name()))
+    result_series = df.groupby(['id', 'id_2'])['incorrect_timestamp'].any()
+    df = df.drop(['start_datetime', 'end_datetime', 'duration', 'incorrect_timestamp'], axis=1)
+  result_series = time_check(df)
+  return result_series
 
-    return pd.Series()
+
